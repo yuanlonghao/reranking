65c4db4b7e8e51e0f91d173ab65775f804c9bb75
longhao-yuan-biz <longhao.yuan@bizreach.co.jp>
2021-09-06 08:10:23 +0900

    refactor metrics

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reranking",
-    version="0.2.6",
+    version="0.2.7",
     author="Longhao Yuan",
     author_email="yuanlonghao1013@gmail.com",
     long_description=long_description,
