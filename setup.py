0c8c5460c01197d85da650209aa218dc9b77e89d
longhao-yuan-biz <longhao.yuan@bizreach.co.jp>
2021-09-05 11:41:45 +0900

    version 0.2.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reranking",
-    version="0.2.2",
+    version="0.2.3",
     author="Longhao Yuan",
     author_email="yuanlonghao1013@gmail.com",
     long_description=long_description,
